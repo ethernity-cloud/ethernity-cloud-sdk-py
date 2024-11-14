d915f934c0147b6ceb52ca9e1cad0a83a374d04b
Iosif Peterfi <iosif.peterfi@gmail.com>
2024-11-14 09:57:49 +0100

    * Updated slash banner to fit the Windows console terminal * Added Docker check before the building process starts * Minor prompt updates

--- ethernity_cloud_sdk_py/commands/pynithy/build.py
+++ ethernity_cloud_sdk_py/commands/pynithy/build.py
@@ -100,7 +100,7 @@ def get_command_output(command):
         command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
     if result.returncode != 0:
-        return ""
+        return None
     return result.stdout.decode("utf-8").strip()
 
 
@@ -110,6 +110,16 @@ def main():
     # Set current directory
     current_dir = os.getcwd()
     VERSION = os.getenv("VERSION")
+    print(f"Checking requirements for {VERSION}")
+    dockerPS = get_command_output("docker version")
+
+    if dockerPS == None:
+        print("""
+Docker daemon is not running. Please start docker to continue.
+More information about installing and running Docker can be founde here: https://docs.docker.com/engine/install/
+""")
+        sys.exit(1)
+  
     print(f"Building {VERSION}")
     # Determine template name based on the blockchain network
     BLOCKCHAIN_NETWORK = os.getenv("BLOCKCHAIN_NETWORK", "Bloxberg_Testnet")
